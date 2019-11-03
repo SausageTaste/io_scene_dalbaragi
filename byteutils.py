b02666952032bfd9e24cbf4fb994354081aeb6cb
Sungmin Woo <woos8899@gmail.com>
2019-11-03 19:57:12 +0900

    Negative values for to_int16 func.

--- byteutils.py
+++ byteutils.py
@@ -6,8 +6,14 @@ def to_bool1(v: bool) -> bytes:
 
 def to_int16(v: int) -> bytes:
     res = struct.pack("<i", int(v))
-    if res[2] or res[3]:
-        raise OverflowError(v)
+
+    if v >= 0:
+        if res[2] != 0 or res[3] != 0:
+            raise OverflowError(v)
+    else:
+        if res[2] != 255 or res[3] != 255:
+            raise OverflowError(v)
+
     return res[0:2]
 
 def to_int32(v: int) -> bytes:
