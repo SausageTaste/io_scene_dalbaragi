60fffcdb65fb37134cb83304defca9cce14c28ef
Sungmin Woo <woos8899@gmail.com>
2024-11-04 18:11:45 +0900

    Update lighting map parser

--- data_exporter.py
+++ data_exporter.py
@@ -118,23 +118,28 @@ class _MaterialParser:
         node_metallic = bsdf.inputs["Metallic"]
 
         material.transparency = alpha_blend
-        material.roughness = node_roughness.default_value
-        material.metallic = node_metallic.default_value
-
         image_node = cls.__find_node_recur_named(cls.__NODE_TEX_IMAGE, bsdf.inputs["Base Color"])
         if image_node is not None:
             material.albedo_map = image_node.image.name
 
         image_node = cls.__find_node_recur_named(cls.__NODE_TEX_IMAGE, node_roughness)
-        if image_node is not None:
+        if image_node is None:
+            material.roughness = node_roughness.default_value
+        else:
+            material.roughness = 1
             material.roughness_map = image_node.image.name
 
-        image_node = cls.__find_node_recur_named(cls.__NODE_TEX_IMAGE, node_roughness)
-        if image_node is not None:
+        image_node = cls.__find_node_recur_named(cls.__NODE_TEX_IMAGE, node_metallic)
+        if image_node is None:
+            material.metallic = node_metallic.default_value
+        else:
+            material.metallic = 1
             material.metallic_map = image_node.image.name
 
         image_node = cls.__find_node_recur_named(cls.__NODE_TEX_IMAGE, bsdf.inputs["Normal"])
-        if image_node is not None:
+        if image_node is None:
+            material.normal_map = ""
+        else:
             material.normal_map = image_node.image.name
 
         return material
